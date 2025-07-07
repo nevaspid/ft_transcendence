678b4fc10fdf2a7ae2ee569ebe65c644ae877cbc
nevaspid <romain.brendle.guido@gmail.com>
2025-07-07 19:02:26 +0200

    feature: added view functions to retrieve data from blockchain and started documentation

--- blockchain-service/contracts/tournament.sol
+++ blockchain-service/contracts/tournament.sol
@@ -37,7 +37,7 @@ contract Tournament is Ownable {
 		tournaments[_tournamentId] = newTournament;
 	}
 
-	function createMatch(uint _matchId, uint _p1Score, uint _p2Score, uint _p1, uint _p2, uint _winner) public onlyOwner {
+	function createMatch(int isTournament, uint _matchId, uint _p1Score, uint _p2Score, uint _p1, uint _p2, uint _winner) public onlyOwner {
 		MatchData memory newMatch = MatchData({
 			matchId: _matchId,
 			p1Score: _p1Score,
@@ -46,5 +46,53 @@ contract Tournament is Ownable {
 			p2:	_p2,
 			winner:	_winner
 		});
+
+		if(isTournament >= 0) {
+			addMatchInTournament(uint(isTournament));
+		}
+
+		matches[_matchId] = newMatch;
 	}
+
+	function getMatchData(uint _matchId) public view returns (
+    uint matchId,
+    uint p1,
+    uint p1Score,
+    uint p2,
+    uint p2Score,
+    uint winner
+	) {
+		require(matches[_matchId].matchId != 0, "Match does not exist");
+		MatchData memory matchData = matches[_matchId];
+		return (matchData.matchId, matchData.p1, matchData.p1Score, matchData.p2, matchData.p2Score, matchData.winner);
+	}
+
+	function getTournamentData(uint _tournamentId) public view returns (
+	string memory tournamentName,
+	uint tournamentId,
+	uint nbPlayers,
+	uint[] memory matchIds
+	) {
+		require(tournaments[_tournamentId].tournamentId != 0, "Tournament does not exist");
+		TournamentData memory tournamentData = tournaments[_tournamentId];
+		return (tournamentData.tournamentName, tournamentData.tournamentId, tournamentData.nbPlayers, tournamentData.matchIds);
+	}
+
+	function getPlayerMatches(uint _player) public view  returns (uint[] memory) {
+    	uint count = 0;
+    	for (uint i = 1; matches[i].matchId != 0; i++) {
+    	    if (matches[i].p1 == _player || matches[i].p2 == _player) {
+    	        count++;
+    	    }
+    	}
+
+    	uint[] memory playerMatches = new uint[](count);
+    	uint idx = 0;
+    	for (uint i = 1; matches[i].matchId != 0; i++) {
+    	    if (matches[i].p1 == _player || matches[i].p2 == _player) {
+    	        playerMatches[idx++] = matches[i].matchId;
+    	    }
+    	}
+    	return playerMatches;
+	} //! a opti si necessaire mais fonctionnel
 }
--- blockchain-service/tasklist.sol
+++ blockchain-service/tasklist.sol
@@ -1,15 +1,24 @@
-//NEEDED FUNCTIONS
+//NEEDED FUNCTIONS, FUTURE DOCUMENTATION
 
 
-function createTournament(string _name, uint _tournamentId, uint _nbPlayers) public onlyOwner; //CREATES AND STORES A NEW TOURNAMENT IN : tournaments[]
+//! FIRST MATCH MUST BE ID 1, NOT 0, AS IT IS USED TO CHECK IF A MATCH EXISTS
+//! FIRST TOURNAMENT MUST BE ID 1, NOT 0, AS IT ISUSED TO CHECK IF A TOURNAMENT EXISTS
 
-function createMatch(uint _matchId, Score _scores, uint _p1, uint _p2, uint _winner) public onlyOwner; //CREATES AND STORES A NEW MATCH IN : matchs[] THAT WILL BE ADDED TO tournaments.matchIds IF IS A TOURNAMENT MATCH
 
-function addMatchInTournament(uint _tournamentId) public onlyOwner; //ADDS NEW MATCH ID TO THE MATCH ARRAY IN SPECIFIC TOURNAMENT
+function createTournament(string _name, uint _tournamentId, uint _nbPlayers) public onlyOwner;
+//CREATES AND STORES A NEW TOURNAMENT IN : tournaments[]
 
-function getMatchData(uint _matchId) public view; //RETRIEVES DATA OF A MATCH BY ID
+function createMatch(int _isTournament, uint _matchId, Score _scores, uint _p1, uint _p2, uint _winner) public onlyOwner;
+//CREATES AND STORES A NEW MATCH IN : matchs[] THAT WILL BE ADDED TO tournaments.matchIds IF IS A TOURNAMENT MATCH (seen with the _isTournament value reprensenting tournament id)
 
-function getTournamentData(uint _tournamentId) public view; //RETRIEVES DATA OF A TOURNAMENT BY ID
+function addMatchInTournament(uint _tournamentId) public onlyOwner;
+//ADDS NEW MATCH ID TO THE MATCH ARRAY IN SPECIFIC TOURNAMENT
 
-function getPlayerMatches(uint _player) public view; //RETRIEVES ALL PLAYER INVOLVED MATCHS
+function getMatchData(uint _matchId) public view;
+//RETRIEVES DATA OF A MATCH BY ID
 
+function getTournamentData(uint _tournamentId) public view;
+//RETRIEVES DATA OF A TOURNAMENT BY ID
+
+function getPlayerMatches(uint _player) public view
+//RETRIEVES ALL PLAYER INVOLVED MATCHS
