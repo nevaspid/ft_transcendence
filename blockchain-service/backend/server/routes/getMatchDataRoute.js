d1c4f9d2053bd432b609f96a231457084232943b
nevaspid <romain.brendle.guido@gmail.com>
2025-07-27 14:57:27 +0200

    feat: getMatchData route done

--- blockchain-service/backend/server/routes/getMatchDataRoute.js
+++ blockchain-service/backend/server/routes/getMatchDataRoute.js
@@ -1,2 +1,9 @@
 import { getMatchDataHandler } from '../controllers/getMatchDataHandler.js'
 import { getMatchData } from '../schemas/getMatchData.js'
+
+export async function matchRoutes(fastify) {
+  fastify.get('/matches/:id', {
+    schema: getMatchData,
+    handler: getMatchDataHandler
+  })
+}
