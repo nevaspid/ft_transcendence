dfa2800726f77bf016597f29d6f4d60fc857df72
nevaspid <romain.brendle.guido@gmail.com>
2025-08-07 16:30:07 +0200

    feat: server.js

---
+++ game-service/server/server.js
@@ -0,0 +1,14 @@
+import Fastify from "fastify"
+
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.listen({ port: 3101 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
